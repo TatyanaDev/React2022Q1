a44f2b7d4d0c8e29a8017420dde2e01599e0f233
Tatyana404 <kot.kate2020@gmail.com>
2022-03-25 21:02:43 +0200

    feat: added main page

---
+++ react_components/src/Pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import Header from './../../Components/Shared/Header';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        Home
+      </>
+    );
+  }
+}
